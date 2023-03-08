0736be13674c80e3e226f4785df24a54897e357c
Lukas Makowski <lukasmakowski4@gmail.com>
2023-03-08 11:41:43 +0100

    Creating a develop-branch and commit a comment in Main

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("GIT TEST! 2. Commit");
         // shelve
+        // Now creating a develop-branch and commit this comment
     }
 }
