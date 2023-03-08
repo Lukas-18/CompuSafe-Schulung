3766bfee1f8366f4544a0b5167c4d74e5d8447fb
Lukas Makowski <lukasmakowski4@gmail.com>
2023-03-08 13:23:39 +0100

    Aufgabe 1 erstellt + Lösung

---
+++ src/aufgabe_1/Aufgabe_1.java
@@ -0,0 +1,30 @@
+package aufgabe_1;
+
+public class Aufgabe_1 {
+
+//    a) 5
+// b) 5.0
+// c) 4L
+// d) 3 + 9
+// e) 2 + 6.0
+// f) 5 + 3.0f
+// g) 3d + 1.0f
+// h) 6L * 3
+// i) 7 / 2
+// j) 9 / 2.0
+// k) int i = 2; i--;
+
+    /*
+    a) byte     5
+    b) float    5.0
+    c) long     4L
+    d) byte     12
+    e) float    8.0
+    f) float    8.0f
+    g) float    4.0f
+    h) long     18L
+    i) float    3.5
+    j) float    4.5
+    k) int      1
+     */
+}
