a2b6975f6d9f1c47240a02ccfe36783a9ece1493
Lukas Makowski <lukasmakowski4@gmail.com>
2023-03-09 16:24:52 +0100

    Aufgabe 6 - Ratespiel, mit rekursiver Lösung.

---
+++ src/aufgabe_6_Ratespiel/Ratespiel.java
@@ -0,0 +1,31 @@
+package aufgabe_6_Ratespiel;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ratespiel {
+
+    static int randomNumber = new Random().nextInt(100);
+    public static void main(String [] args){
+        readNumber();
+    }
+
+    static void readNumber(){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Number: ");
+        int guess = scan.nextInt();
+
+        if(guess == randomNumber){
+            System.out.println("Gewonnen!");
+        } else {
+            if (guess < randomNumber) {
+                System.out.println("Größer!");
+            } else {
+                System.out.println("Kleiner!");
+            }
+            readNumber();
+        }
+    }
+
+}
